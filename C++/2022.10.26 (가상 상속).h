affcb4008ba736c9bfd58dbd113cf430adce9e48
jungseongmin <rlaekrud872695@gmail.com>
2022-10-27 21:46:08 +0900

    2022.10.27 (스마트 포인터)

---
+++ C++/2022.10.26 (가상 상속).h
@@ -0,0 +1,148 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+// 다중 상속이란?
+// 어떤 클래스가 하나 이상의 상위 클래스로부터 여러 가지 행동이나 특징을
+// 상속받을 수 있는 상속입니다.
+/*
+class Keyboard
+{
+public :
+	Keyboard()
+	{
+		cout << "keyBoard 호출" << endl;
+	}
+
+	void Input()
+	{
+		cout << "키보드 입력" << endl;
+	}
+
+	int key;
+};
+
+class Mouse
+{
+public :
+	Mouse()
+	{
+		cout << "Mouse 호출" << endl;
+	}
+
+	void Input()
+	{
+		cout << "마우스 입력" << endl;
+	}
+
+	int sensor;
+};
+
+class Computer : public Keyboard, public Mouse
+{
+	Computer(int _key, int _sensor)
+	{
+		Keyboard::key = _key;
+		Mouse::sensor = _sensor;
+
+		cout << Keyboard::key << endl;
+		cout << Mouse::sensor << endl;
+	}
+};
+*/
+
+// 가상 상속
+/*
+class A
+{
+public :
+	A()
+	{
+		cout << "A 클래스 호출" << endl;
+	}
+};
+
+class B : virtual public A
+{
+public :
+	B()
+	{
+		cout << "B 클래스 호출" << endl;
+	}
+};
+
+class C : virtual public A
+{
+public :
+	C()
+	{
+		cout << "C 클래스 호출" << endl;
+	}
+};
+
+class D : public B, public C
+{
+public :
+	D()
+	{
+		cout << "D 클래스 호출" << endl;
+	}
+};
+*/
+
+int main()
+{
+	// 다중 상속
+	/*
+	Computer computer(1,40);
+
+	// 다중 상속일 때 같은 이름의 함수를 사용하려면 클래스를 명시하고
+	// 범위 지정 연산자를 사용해서 함수를 호출해야 합니다.
+	computer.Keyboard::Input(); // keyboard 클래스의 Input();
+	computer.Mouse::Input();
+	*/
+
+	// 다이아몬드 상속
+	/*
+	// 하나의 자식 클래스가 상속받는 서로 다른 부모 클래스들이,
+	// 같은 조부모 클래스를 상속받는 구조입니다.
+	D d;
+
+	// 가상 상속
+	*/
+
+	// 나머지
+	int array[10] = { 0, };
+	int result = 0;
+	int input = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> input;
+		array[i] = input % 42;
+	}
+
+	for (int i = 0; i <= 9; i++)
+	{
+		int count = 0;
+		// [1][2][3][4][5][6][7][8][9]
+		// 1  2  3  4  5  6  7  8  9 10
+		for (int j = i + 1; j <= 9; j++)
+		{
+			if (array[i] == array[j])
+			{
+				count++;
+			}
+		}
+
+		if (count == 0)
+		{
+			result++;
+		}
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
--- C++/C++.cpp
+++ C++/C++.cpp
@@ -1,147 +1,123 @@
 ﻿#include <iostream>
+#include <memory>
 
 using namespace std;
 
-// 다중 상속이란?
-// 어떤 클래스가 하나 이상의 상위 클래스로부터 여러 가지 행동이나 특징을
-// 상속받을 수 있는 상속입니다.
+// 스마트 포인터란?
 /*
-class Keyboard
+// 포인터처럼 동작하는 클래스 템플릿으로 사용이 끝난 메모리를
+// 자동으로 해제하는 포인터입니다.
+
+
+class Player
 {
 public :
-	Keyboard()
+	Player()
 	{
-		cout << "keyBoard 호출" << endl;
+		cout << "Player 생성" << endl;
 	}
 
-	void Input()
+	~Player()
 	{
-		cout << "키보드 입력" << endl;
-	}
-
-	int key;
-};
-
-class Mouse
-{
-public :
-	Mouse()
-	{
-		cout << "Mouse 호출" << endl;
-	}
-
-	void Input()
-	{
-		cout << "마우스 입력" << endl;
-	}
-
-	int sensor;
-};
-
-class Computer : public Keyboard, public Mouse
-{
-	Computer(int _key, int _sensor)
-	{
-		Keyboard::key = _key;
-		Mouse::sensor = _sensor;
-
-		cout << Keyboard::key << endl;
-		cout << Mouse::sensor << endl;
+		cout << "Player 소멸" << endl;
 	}
 };
 */
 
-// 가상 상속
-/*
-class A
+class Person
 {
-public :
-	A()
+public:
+	// shared_ptr로 선언할 경우 순환 참조가 발생합니다.
+	// weak_ptr로 선언할 경우 순환 참조를 예방할 수 있습니다.
+	weak_ptr<Person> person;
+
+	Person()
 	{
-		cout << "A 클래스 호출" << endl;
+		cout << "생성" << endl;
 	}
+
+	~Person()
+	{
+		cout << "소멸" << endl;
+	}
+
 };
 
-class B : virtual public A
-{
-public :
-	B()
-	{
-		cout << "B 클래스 호출" << endl;
-	}
-};
-
-class C : virtual public A
-{
-public :
-	C()
-	{
-		cout << "C 클래스 호출" << endl;
-	}
-};
-
-class D : public B, public C
-{
-public :
-	D()
-	{
-		cout << "D 클래스 호출" << endl;
-	}
-};
-*/
 
 int main()
 {
-	// 다중 상속
+	// shared_ptr 포인터
 	/*
-	Computer computer(1,40);
+	// 어떤 하나의 객체를 참조하는 스마트 포인터의 갯수를 참조하는 포인터입니다.
+	// 참조하고 있는 스마트 포인터의 갯수를 참조 카운트라고 합니다.
+	
+	// 참조 카운트란?
+	// 해당 메모리를 참조하는 포인터가 몇개가 있는지 나타내는 값입니다.
+	// 참조하는 포인터가 추가되면 1씩 증가하고 참조하는 포인터가 끊어지면 1씩 감소합니다.
 
-	// 다중 상속일 때 같은 이름의 함수를 사용하려면 클래스를 명시하고
-	// 범위 지정 연산자를 사용해서 함수를 호출해야 합니다.
-	computer.Keyboard::Input(); // keyboard 클래스의 Input();
-	computer.Mouse::Input();
+	// 첫번째로 초기화하는 방법
+	// shared_ptr<int> ptr1(new int(5));
+	// 두번째로 초기화하는 방법
+	// shared_ptr<int> ptr1 = make_shared<int>(5);
+
+	// shared_ptr에서 참조 카운트를 확인하는 방법
+	// ptr1.use_count()
+
+	shared_ptr<Player> ptr1(new Player());
+	
+	cout << "ptr1 참조 카운트 : " << ptr1.use_count() << endl;
+
+	shared_ptr<Player> ptr2 = ptr1;
+
+	cout << "ptr1 참조 카운트 : " << ptr2.use_count() << endl;
+
+	// delete ptr2;
+	// delete ptr1;
 	*/
 
-	// 다이아몬드 상속
+	// unique_ptr 포인터
 	/*
-	// 하나의 자식 클래스가 상속받는 서로 다른 부모 클래스들이,
-	// 같은 조부모 클래스를 상속받는 구조입니다.
-	D d;
+	// 하나의 스마트 포인터만 가리킬 수 있는 포인터입니다.
+	// shared_ptr 참조 카운트를 1 이상 넘길 수 없습니다.
 
-	// 가상 상속
+	unique_ptr<Player> ptr1(new Player());
+
+	// ptr1이 가지고 있던 메모리 소유권(동적으로 할당한 메모리)를
+	// ptr2에게 메모리 소유권을 넘겨주는 과정입니다.
+
+	unique_ptr<Player> ptr2 = move(ptr1);
+
+	// reset : 메모리 영역을 삭제할 수 있는 함수
+	ptr2.reset();
+
+	// unique_ptr<int> ptr3 = ptr1; ERROR
 	*/
 
-	// 나머지
-	int array[10] = { 0, };
-	int result = 0;
-	int input = 0;
+	// weak_ptr 포인터
+	// 하나 이상의 shared_ptr 인스턴스가 소유하는 객체에 대한 접근을
+	// 제공하지만, 참조 카운트에 포함되지 않는 스마트 포인터입니다.
 
-	for (int i = 0; i < 10; i++)
-	{
-		cin >> input;
-		array[i] = input % 42;
-	}
+	// 만약에 서로가 상대를 가리키는 shared_ptr을 가지고 있다면,
+	// 참조 횟수는 절대 1 이하로 떨어지지 않습니다.
+	// shared_ptr이 해제가 되지 않는 문제가 발생합니다.
 
-	for (int i = 0; i <= 9; i++)
-	{
-		int count = 0;
-		// [1][2][3][4][5][6][7][8][9]
-		// 1  2  3  4  5  6  7  8  9 10
-		for (int j = i + 1; j <= 9; j++)
-		{
-			if (array[i] == array[j])
-			{
-				count++;
-			}
-		}
+	// 순환 참조
+	// 서로 상대를 참조하는 상황입니다.
 
-		if (count == 0)
-		{
-			result++;
-		}
-	}
+	// shared_ptr 인스턴스 사이에 순환 참조를 제거하기 위해 사용하는
+	// 스마트포인터입니다.
 
-	cout << result << endl;
+	shared_ptr<Person> obj1(new Person());
+	shared_ptr<Person> obj2(new Person());
+
+	cout << "obj1 참조 카운트 : " << obj1.use_count() << endl;
+	cout << "obj2 참조 카운트 : " << obj2.use_count() << endl;
+
+	obj1->person = obj2;
+	obj2->person = obj1;
+
+	cout << "obj1 참조 카운트 : " << obj1.use_count() << endl;
+	cout << "obj2 참조 카운트 : " << obj2.use_count() << endl;
 
 	return 0;
 }
